64b4522771f9639880fef805dc28dd2143740cff
shumy <micaelpedrosa@gmail.com>
2020-04-29 22:44:27 +0100

    Support for query retrieve with direct references

--- src/main/kotlin/dr/adaptor/MultiQueryExecutor.kt
+++ src/main/kotlin/dr/adaptor/MultiQueryExecutor.kt
@@ -3,26 +3,61 @@ package dr.adaptor
 import dr.spi.IQueryExecutor
 import dr.spi.IResult
 import dr.spi.Rows
+import org.jooq.Field
+import org.jooq.Record
 import org.jooq.Select
+
+class QStruct(val main: Select<*>) {
+  val inverted = linkedMapOf<String, QStruct>()
+}
 
 class MultiQueryExecutor(private val struct: QStruct): IQueryExecutor {
   override fun exec(params: Map<String, Any>): IResult {
     params.forEach { struct.main.bind(it.key, it.value) }
 
     println(struct.main.sql)
-    println(struct.main.fetch())
-    return SQLResult(emptyList())
+
+    val data = TData()
+    val directResult = struct.main.fetch()
+    directResult.forEach { data.process(it) }
+
+    return SQLResult(data)
   }
 }
 
-data class QStruct(val main: Select<*>) {
-  val inverted = linkedMapOf<String, QStruct>()
-}
-
-private class SQLResult(val raw: Rows): IResult {
+private class SQLResult(val data: TData): IResult {
   override fun <T : Any> get(name: String): T {
     TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
   }
 
-  override fun raw() = raw
+  override fun raw() = data.rows
+}
+
+
+/* ------------------------- helpers -------------------------*/
+class TData {
+  val rows = mutableListOf<LinkedHashMap<String, Any?>>()
+
+  fun process(record: Record) {
+    val row = linkedMapOf<String, Any?>()
+    record.fields().forEach {
+      val value = it.getValue(record)
+      if (it.name.startsWith('.')) row.processJoin(it.name, value) else row[it.name] = value
+    }
+
+    rows.add(row)
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  private fun LinkedHashMap<String, Any?>.processJoin(name: String, value: Any?) {
+    val splits = name.split('.').drop(1)
+    print(splits)
+
+    var place = this
+    for (position in splits.dropLast(1)) {
+      place = place.getOrPut(position) { linkedMapOf<String, Any?>() } as LinkedHashMap<String, Any?>
+    }
+
+    place[splits.last()] = value
+  }
 }
